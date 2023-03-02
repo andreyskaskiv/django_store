2f3b608ad1cddb257dd1f5238d6a15c60e8f12ed
andrey <andreyskaskiv.py@gmail.com>
2023-03-02 22:39:55 +0200

    7. added models.py, admin.py. A bug with a picture
    a bug with a picture, they are not displayed, everything else works

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from products.models import Product, ProductCategory
 
 def index(request):
     context = {'title': 'Store'}
@@ -11,32 +11,8 @@ def index(request):
 def products(request):
     context = {
         'title': 'Store - Catalog',
-        'products': [
-            {
-                'image': '/static/vendor/img/products/Adidas-hoodie.png',
-                'name': 'adidas Originals Black Monogram Hoodie',
-                'price': 100,
-                'description': 'Soft fabric for sweatshirts. Style and comfort is a way of life.'
-            },
-            {
-                'image': '/static/vendor/img/products/Blue-jacket-The-North-Face.png',
-                'name': 'The North Face blue jacket',
-                'price': 200,
-                'description': 'Smooth fabric. Waterproof coating. Lightweight and warm down fill.'
-            },
-            {
-                'image': '/static/vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png',
-                'name': 'Nike Black Heritage Backpack',
-                'price': 300,
-                'description': 'Plush material. Comfortable and soft.'
-            },
-            {
-                'image': '/static/vendor/img/products/Black-Dr-Martens-shoes.png',
-                'name': 'Dr Martens 1461 Bex Black 3-Eye Platform Shoes',
-                'price': 400,
-                'description': 'Smooth leather upper. Natural material.'
-            },
-        ]
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all()
     }
     return render(request,
                   'products/products.html',
--- store/urls.py
+++ store/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 from products.views import index
 from products.views import products
 
@@ -25,3 +28,6 @@ urlpatterns = [
     path('products/', products, name='products'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns == static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
