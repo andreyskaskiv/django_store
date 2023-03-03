213da7e40d02086720390e401af24a910750595f
andrey <andreyskaskiv.py@gmail.com>
2023-03-03 19:33:23 +0200

    15. create profile.html

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from users.views import login, register
+from users.views import login, register, profile
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', login, name='login'),  # ../users/login
     path('register/', register, name='register'),  # ../users/register
+    path('profile/', profile, name='profile'),  # ../users/profile
+
 ]
