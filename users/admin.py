3939f639f89e1444d22be07432c0e33d9fd56f8a
andrey <andreyskaskiv.py@gmail.com>
2023-03-04 17:35:35 +0200

    28. IndexView ------ Function-Based Views or Class-Based Views ------

--- users/admin.py
+++ users/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from products.admin import BasketAdmin
 from users.models import User
 
+
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username',)
