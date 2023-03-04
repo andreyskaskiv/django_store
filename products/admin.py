b19af01e807a164305372e36c76319cfe8b93ac5
andrey <andreyskaskiv.py@gmail.com>
2023-03-04 17:11:53 +0200

    27. added UserAdmin and ProductAdmin

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 
-from products.models import Product, ProductCategory
+from products.models import Basket, Product, ProductCategory
 
-admin.site.register(Product)
 admin.site.register(ProductCategory)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """How and what will be reflected in the admin panel"""
+
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('image', 'name', 'description', ('price', 'quantity'), 'category')
+    readonly_fields = ('description',)
+    search_fields = ('name', 'category')
+    ordering = ('-name', '-price', '-quantity',)
+
+
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = ('product', 'quantity', 'created_timestamp')
+    readonly_fields = ('created_timestamp',)
+    extra = 0
--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
+from products.admin import BasketAdmin
 from users.models import User
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username',)
+    inlines = (BasketAdmin,)
