fa5e03ff7322495b67e25f4ff0271df379171ee3
andrey <andreyskaskiv.py@gmail.com>
2023-03-04 17:11:53 +0200

    27. added UserAdmin and ProductAdmin

--- products/models.py
+++ products/models.py
@@ -7,6 +7,10 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=128, unique=True)
     description = models.TextField(null=True, blank=True)
 
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
     def __str__(self):
         return self.name
 
@@ -18,6 +22,10 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images', null=True, blank=True)
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
 
     def __str__(self):
         return f'Product: {self.name} | Category: {self.category.name}'
