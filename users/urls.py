7bd9083b3910e02e2c9df2af31ad84938bb22a6a
andrey <andreyskaskiv.py@gmail.com>
2023-03-05 11:53:19 +0200

    31. UserProfileView

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,14 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 
-from users.views import login, UserRegistrationView, profile, logout
+from users.views import login, UserRegistrationView, UserProfileView, logout
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', login, name='login'),  # ../users/login
     path('register/', UserRegistrationView.as_view(), name='register'),  # ../users/register
-    path('profile/', profile, name='profile'),  # ../users/profile
+    path('profile/<int:pk>/', login_required(UserProfileView.as_view()), name='profile'),  # ../users/profile
     path('logout/', logout, name='logout'),
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,7 @@
 from django.contrib import auth, messages
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 
 from products.models import Basket
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
@@ -33,25 +32,31 @@ class UserRegistrationView(CreateView):
     template_name = 'users/register.html'
     success_url = reverse_lazy('users:login')
 
+    def get_context_data(self, **kwargs):
+        context = super(UserRegistrationView, self).get_context_data()
+        context['title'] = 'Store - Registration'
+        return context
+
 
 # messages.success(request, 'You have successfully registered!')
 
+class UserProfileView(UpdateView):
+    model = User
+    form_class = UserProfileForm
+    template_name = 'users/profile.html'
+    title = 'Store - Profile'
 
-@login_required
-def profile(request):
-    if request.method == 'POST':
-        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Data updated!')
-            return HttpResponseRedirect(reverse('users:profile'))
-    else:
-        form = UserProfileForm(instance=request.user)
+    def get_success_url(self):
+        return reverse_lazy('users:profile', args=(self.object.id,))
 
-    context = {'title': 'Store - Profile',
-               'form': form,
-               'baskets': Basket.objects.filter(user=request.user)}
-    return render(request, 'users/profile.html', context)
+    def get_context_data(self, **kwargs):
+        context = super(UserProfileView, self).get_context_data()
+        context['title'] = 'Store - Profile'
+        context['baskets'] = Basket.objects.filter(user=self.object)
+        return context
+
+
+# messages.success(request, 'Data updated!')
 
 
 def logout(request):
