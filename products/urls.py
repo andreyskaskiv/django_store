e8c3b1145b066cfcd1b3d933d9e812834f23f026
andrey <andreyskaskiv.py@gmail.com>
2023-03-06 14:14:12 +0200

    33. ProductDetailView

--- products/urls.py
+++ products/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from products.views import ProductsListView, basket_add, basket_remove
+from products.views import ProductsListView, ProductDetailView, basket_add, basket_remove
 
 app_name = 'products'
 
 urlpatterns = [
     path('', ProductsListView.as_view(), name='index'),
     path('category/<int:category_id>/', ProductsListView.as_view(), name='category'),
+    path('product_details/<int:pk>/', ProductDetailView.as_view(), name='product_details'),
     path('page/<int:page>/', ProductsListView.as_view(), name='paginator'),
 
     path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),
