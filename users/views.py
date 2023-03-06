8ca391574a515b97e40ae6df5fda3c317d8293df
andrey <andreyskaskiv.py@gmail.com>
2023-03-06 15:51:18 +0200

    35. TitleMixin

--- users/views.py
+++ users/views.py
@@ -3,34 +3,37 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView
 
+from common.views import TitleMixin
 from products.models import Basket
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from users.models import User
 
 
-class UserLoginView(SuccessMessageMixin, LoginView):
+class UserLoginView(TitleMixin, SuccessMessageMixin, LoginView):
     template_name = 'users/login.html'
     form_class = UserLoginForm
     success_message = 'You have successfully logged in!'
+    title = 'Login'
 
 
-class UserRegistrationView(SuccessMessageMixin, CreateView):
+class UserRegistrationView(TitleMixin, SuccessMessageMixin, CreateView):
     model = User
     form_class = UserRegistrationForm
     template_name = 'users/register.html'
+    title = 'Store - Registration'
     success_url = reverse_lazy('users:login')
     success_message = 'You have successfully registered!'
 
     def get_context_data(self, **kwargs):
         context = super(UserRegistrationView, self).get_context_data()
-        context['title'] = 'Store - Registration'
         return context
 
 
-class UserProfileView(SuccessMessageMixin, UpdateView):
+class UserProfileView(TitleMixin, SuccessMessageMixin, UpdateView):
     model = User
     form_class = UserProfileForm
     template_name = 'users/profile.html'
+    title = 'Store - Profile'
     success_message = 'Data updated!'
 
     def get_success_url(self):
@@ -38,6 +41,5 @@ class UserProfileView(SuccessMessageMixin, UpdateView):
 
     def get_context_data(self, **kwargs):
         context = super(UserProfileView, self).get_context_data()
-        context['title'] = 'Store - Profile'
         context['baskets'] = Basket.objects.filter(user=self.object)
         return context
