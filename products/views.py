2fe0a59d9e586aa51b4bb77fff6f5b701553a8c3
andrey <andreyskaskiv.py@gmail.com>
2023-03-06 14:14:12 +0200

    33. ProductDetailView

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
+from django.views.generic import DetailView
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
 
@@ -28,6 +29,18 @@ class ProductsListView(ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(ProductsListView, self).get_context_data()
         context['title'] = 'Store - Catalog'
+        context['categories'] = ProductCategory.objects.all()
+        return context
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'products/product_detail.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ProductDetailView, self).get_context_data()
+        context['title'] = 'Store - Product details'
+
         context['categories'] = ProductCategory.objects.all()
         return context
 
