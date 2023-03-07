8cf978270015e70b9c44c9d8d759c354cb12c957
andrey <andreyskaskiv.py@gmail.com>
2023-03-07 19:00:50 +0200

    45. refactor admin panel

--- users/admin.py
+++ users/admin.py
@@ -6,12 +6,26 @@ from users.models import User, EmailVerification
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username',)
+    list_display = ('username',
+                    'first_name',
+                    'last_name',
+                    'email',
+                    'username',
+                    'image',
+                    )
+    list_filter = ('username',
+                   'email',
+                   )
     inlines = (BasketAdmin,)
 
 
 @admin.register(EmailVerification)
 class EmailVerificationAdmin(admin.ModelAdmin):
-    list_display = ('code', 'user', 'expiration')
-    fields = ('code', 'user', 'expiration', 'created')
+    list_display = ('code',
+                    'user',
+                    'expiration')
+    fields = ('code',
+              'user',
+              'expiration',
+              'created')
     readonly_fields = ('created',)
