c45da3f5acc9e21de5c9edb499769ae281b82118
andrey <andreyskaskiv.py@gmail.com>
2023-03-10 16:45:31 +0200

    49. bag fix search

--- products/admin.py
+++ products/admin.py
@@ -12,7 +12,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'quantity', 'category')
     fields = ('image', 'name', 'description', ('price', 'quantity'), 'category')
     readonly_fields = ('description',)
-    search_fields = ('name', 'category')
+    search_fields = ('name',)
     ordering = ('-name', '-price', '-quantity',)
 
 
