12be6fcdba19cd37f9db262e2f1e1078a06c5fa2
andrey <andreyskaskiv.py@gmail.com>
2023-03-12 17:48:45 +0200

    58. created OrderForm, Order

--- orders/views.py
+++ orders/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView
 
 from common.views import TitleMixin
+from orders.forms import OrderForm
 
 
-class OrderCreateView(TitleMixin, TemplateView):
+class OrderCreateView(TitleMixin, CreateView):
     template_name = 'orders/order-create.html'
+    form_class = OrderForm
     success_url = reverse_lazy('orders:order_create')
     title = 'Store - Checkout'
